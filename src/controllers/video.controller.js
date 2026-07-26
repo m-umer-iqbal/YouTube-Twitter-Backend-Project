503e6c6b53102ae4c354bbabc66ab8b2cecf4391
Umer Iqbal <official.umer.iqbal@gmail.com>
2026-07-26 23:53:29 +0500

    Add get video functionality, improve the code by seprate the logic of videoId checking

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -6,6 +6,23 @@ import ApiResponse from "../utils/ApiResponse.js"
 import asyncHandler from "../utils/asyncHandler.js"
 import uploadOnCloudinary from "../utils/cloudinary.js"
 
+// Helper function
+const checkVideoId = async (videoId) => {
+    if (!videoId || videoId === "") {
+        throw new ApiError(400, "Video Id is required.");
+    } else if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Video Id is not valid.");
+    }
+
+    const video = await Video.findById(videoId);
+
+    if (!video) {
+        throw new ApiError(404, "Video does not exist.");
+    }
+
+    return video;
+};
+
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
     //TODO: get all videos based on query, sort, pagination
@@ -70,8 +87,15 @@ const publishAVideo = asyncHandler(async (req, res) => {
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-    //TODO: get video by id
+    const { videoId } = req.params;
+
+    const video = await checkVideoId(videoId);
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, video, "Video fetched successfully.")
+        );
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
@@ -83,17 +107,7 @@ const updateVideo = asyncHandler(async (req, res) => {
 const deleteVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params;
 
-    if (!videoId || videoId === "") {
-        throw new ApiError(400, "Video Id is required.");
-    } else if (!isValidObjectId(videoId)) {
-        throw new ApiError(400, "Video Id is not valid.");
-    }
-
-    const video = await Video.findById(videoId);
-
-    if (!video) {
-        throw new ApiError(404, "Video does not exist.");
-    }
+    const video = await checkVideoId(videoId);
 
     if (video.owner.toString() !== req.user?._id.toString()) {
         throw new ApiError(403, "You are not authorized to delete this video.");
@@ -111,17 +125,7 @@ const deleteVideo = asyncHandler(async (req, res) => {
 const togglePublishStatus = asyncHandler(async (req, res) => {
     const { videoId } = req.params;
 
-    if (!videoId || videoId === "") {
-        throw new ApiError(400, "Video Id is required.");
-    } else if (!isValidObjectId(videoId)) {
-        throw new ApiError(400, "Video Id is not valid.");
-    }
-
-    const video = await Video.findById(videoId);
-
-    if (!video) {
-        throw new ApiError(400, "Video does not exist.");
-    }
+    const video = await checkVideoId(videoId);
 
     video.isPublished = !video.isPublished;
 
