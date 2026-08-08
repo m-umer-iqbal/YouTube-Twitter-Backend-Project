8865240efbb6fa5bff8d9bdac26015eb2a74b4de
Umer Iqbal <official.umer.iqbal@gmail.com>
2026-08-08 06:55:08 +0500

    Add the createPlaylist and getUserPlaylists functionalites

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -3,18 +3,86 @@ import Playlist from "../models/playlist.model.js"
 import ApiError from "../utils/ApiError.js"
 import ApiResponse from "../utils/ApiResponse.js"
 import asyncHandler from "../utils/asyncHandler.js"
+import User from "../models/user.model.js"
 
+// Helper function
+const checkPlaylistId = async (playlistId) => {
+    if (!playlistId || playlistId === "") {
+        throw new ApiError(400, "Playlist Id is required.");
+    } else if (!isValidObjectId(playlistId)) {
+        throw new ApiError(400, "Playlist Id is not valid.");
+    }
+
+    const playlist = await Playlist.findById(playlistId);
+
+    if (!playlist) {
+        throw new ApiError(404, "Playlist does not exist.");
+    }
+
+    return playlist;
+};
 
 const createPlaylist = asyncHandler(async (req, res) => {
-    const { name, description } = req.body
+    const { name, description } = req.body;
 
-    //TODO: create playlist
+    if (!name || name === "") {
+        throw new ApiError(400, "Playlist name is required.");
+    } else if (!description || description === "") {
+        throw new ApiError(400, "Playlist description is required.");
+    }
+
+    const existedPlaylist = await Playlist.findOne({ name });
+
+    if (existedPlaylist) {
+        throw new ApiError(409, "Playlist already exist.");
+    }
+
+    const playlist = await Playlist.create({
+        name,
+        description,
+        owner: req.user?._id
+    });
+
+    if (!playlist) {
+        throw new ApiError(500, "Playlist not created.");
+    }
+
+    return res
+        .status(201)
+        .json(
+            new ApiResponse(200, playlist, "Playlist created successfully.")
+        );
 })
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
-    const { userId } = req.params
-    //TODO: get user playlists
-})
+    const { userId } = req.params;
+
+    if (!userId || userId === "") {
+        throw new ApiError(400, "User Id is required.");
+    } else if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "User Id is not valid.");
+    }
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+        throw new ApiError(404, "User does not exist.");
+    }
+
+    const playlists = await Playlist.find({
+        owner: userId
+    });
+
+    if (!playlists || playlists.length === 0) {
+        throw new ApiError(404, "No playlist exist.");
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, playlists, "Playlists fetched successfully.")
+        );
+});
 
 const getPlaylistById = asyncHandler(async (req, res) => {
     const { playlistId } = req.params
